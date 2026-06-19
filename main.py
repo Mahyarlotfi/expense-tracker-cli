e34ca0739c2fad271d3814299c94885281014838
Mahyar Lotfi <mahyarlotfi@gmail.com>
2026-06-19 18:21:28 +0330

    Add persistent expense storage with JSON read/write and error handling

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import json
 
+
+try:
+    with open("expenses.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+except (FileNotFoundError, json.JSONDecodeError):
+    data = []
+
 amount = float(input("Please enter amount: "))
 category = input("Please enter category: ")
 description = input("Please enter description: ")
@@ -10,5 +17,7 @@ expense = {
 	"description": description
 }
 
+data.append(expense)
+
 with open("expenses.json", "w", encoding="utf-8") as file:
-	json.dump(expense, file, indent=4)
+    json.dump(data, file, indent=4)
