d822be634d594ae647e850d1f544a778c6aa7646
Mahyar Lotfi <mahyarlotfi@gmail.com>
2026-06-19 21:18:52 +0330

    Add CLI menu with add and view expense functionality

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
 import json
 
+while True:
+    try:
+        with open("expenses.json", "r", encoding="utf-8") as file:
+            data = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        data = []
+    choice = int(input(
+        "=====================\n"
+        " 1 - Add Expense\n"
+        " 2 - View Expense\n"
+        " 0 - Exit\n"
+        "=====================\n"
+        "Enter your choice: "
+        ))
+    
+    if choice == 1:
+        amount = float(input("Please enter amount: "))
+        category = input("Please enter category: ")
+        description = input("Please enter description: ")
+        expense = {
+	        "amount": amount,
+	        "category": category,
+	        "description": description
+            
+        }
 
-try:
-    with open("expenses.json", "r", encoding="utf-8") as file:
-        data = json.load(file)
-except (FileNotFoundError, json.JSONDecodeError):
-    data = []
-
-amount = float(input("Please enter amount: "))
-category = input("Please enter category: ")
-description = input("Please enter description: ")
-
-expense = {
-	"amount": amount,
-	"category": category,
-	"description": description
-}
-
-data.append(expense)
-
-with open("expenses.json", "w", encoding="utf-8") as file:
-    json.dump(data, file, indent=4)
+        data.append(expense)
+        with open("expenses.json", "w", encoding="utf-8") as file:
+            json.dump(data, file, indent=4)
+            
+    elif choice == 2:
+        for i in data:
+            print(i)
+        
+    elif choice == 0:
+        break
+    
